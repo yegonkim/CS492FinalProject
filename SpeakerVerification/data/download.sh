971d5ff0fb8c505d48e329149c4c47e8c5674e48
Yegon Kim <45692848+yegonkim@users.noreply.github.com>
2021-12-18 14:57:43 +0900

    Add files via upload

---
+++ SpeakerVerification/data/download.sh
@@ -0,0 +1,23 @@
+mkdir -p ./data/RAVDESS
+mkdir -p ./data/Libri
+mkdir -p ./data/voxceleb/
+
+# RAVDESS
+gdown https://drive.google.com/uc?id=1G8Vum5TwDXh6eQ6RjyhEkk6y9GmxQxy4
+# wget -P data/RAVDESS/ https://zenodo.org/record/1188976/files/Audio_Speech_Actors_01-24.zip 
+mv Audio_Speech_Actors_01-24.zip ./ravdess.zip
+unzip -q -o ravdess -d ./data/RAVDESS
+
+#Libri
+wget https://www.openslr.org/resources/12/train-clean-100.tar.gz
+tar -zxf train-clean-100.tar.gz --directory ./data/Libri
+
+# VoxCeleb1
+wget --user voxceleb1912 --password 0s42xuw6 https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partaa
+wget --user voxceleb1912 --password 0s42xuw6 https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partab
+wget --user voxceleb1912 --password 0s42xuw6 https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partac
+wget --user voxceleb1912 --password 0s42xuw6 https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partad
+cat vox1_dev* > vox1_dev_wav.zip
+unzip -q -o vox1_dev_wav.zip -d ./data/voxceleb/
+
+rm -rf *.zip *.tar.gz
